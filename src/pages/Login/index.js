512448ead8569f56ad4536daa1403fd0eb2d871f
Henrique Tavares <ihenrits@gmail.com>
2019-05-29 23:16:39 -0300

    Pagina de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '~/services/api';
 
-import { View, Text } from 'react-native';
+import {
+  Container, Input, Button, ButtonText, Error,
+} from './styles';
 
-// import { Container } from './styles';
+export default class Login extends Component {
+  state = {
+    username: '',
+  };
 
-const Login = () => (
-  <View>
-    <Text>Login</Text>
-  </View>
-);
+  handleSubmit = async () => {
+    const { username } = this.state;
 
-export default Login;
+    try {
+      await api.get(`/users/${username}`);
+    } catch (err) {}
+  };
+
+  render() {
+    const { username } = this.state;
+
+    return (
+      <Container>
+        <Input
+          value={username}
+          onChangeText={text => this.setState({ username: text })}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu usuário"
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
+  }
+}
