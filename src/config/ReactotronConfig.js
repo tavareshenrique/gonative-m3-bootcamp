17ab2d1594094f185cd442bc504ac7ec3f8bb029
Henrique Tavares <ihenrits@gmail.com>
2019-05-30 00:11:26 -0300

    Reactotron + Redux Saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.9.101' }) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect(); // let's connect!
   console.tron = tron;
 
