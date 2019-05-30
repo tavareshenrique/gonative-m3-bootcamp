95cec0d2b25b2390f7ccf971a579eb77bba24ff3
Henrique Tavares <ihenrits@gmail.com>
2019-05-30 00:34:19 -0300

    Pagina de repositorios

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ActivityIndicator, Text } from 'react-native';
 
-import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as RepositoriesActions from '~/store/actions/repositories';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-const Repositories = () => <View />;
+class Repositories extends Component {
+  componentDidMount() {
+    const { loadRepositoriesRequest } = this.props;
 
-export default Repositories;
+    loadRepositoriesRequest();
+  }
+
+  render() {
+    const { repositories } = this.props;
+
+    return (
+      <Container>
+        {repositories.loading ? (
+          <ActivityIndicator size="small" color="#999" />
+        ) : (
+          repositories.data.map(repository => <Text key={repository.id}>{repository.name}</Text>)
+        )}
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  repositories: state.repositories,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Repositories);
